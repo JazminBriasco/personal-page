8f9710c7af6db915316378cd0540ceceec43ed02
JazBri <jaabris@hotmail.com>
2021-03-29 17:05:01 -0300

    cards

--- Views/resume.php
+++ Views/resume.php
@@ -7,6 +7,9 @@
         
         <div class="row ">
             <div class="border-resume-right col-sm-3 resp-col">
+                <div class="subtitle-div"><p class ="card-subtitle">SKILLS</p></div>    
+            
+                <hr>
                 <ul class = "pl-0 list-style-none" >
                     <li>HTML 5</li>
                     <li>CSS3 </li>
@@ -16,6 +19,8 @@
                 </ul> 
             </div>
             <div class="border-resume-left col-sm-2 resp-col">
+            <div class="subtitle-div disp-none"><p class ="card-subtitle">SKILLS</p></div>    
+            <hr >
                 <ul class = "pl-0 list-style-none">
                     <li>PHP </li>
                     <li>SQL</li>
@@ -26,6 +31,7 @@
             </div>
 
             <div class="border-resume col-md-5 offset-md-1 mt-1  ">
+            <p class ="card-subtitle">SKILLS</p><hr>
                 <p><b>Universidad Tecnológica Nacional</b></p>
                 <p>Tecnicatura Universitaria en programación</p>
                 <p><i>Fecha de finalización: 2021</i></p>
@@ -34,6 +40,7 @@
 
         <div class="row ">       
             <div class="border-resume col-md-5 mt-1 ">
+            <p class ="card-subtitle">SKILLS</p><hr>
                 Recientemente finalicé la carrera de <p> <b>'Técnico Universitario en Programación'. </b></p>
                 <p>Busco formar parte de un equipo con un buen ambiente de compañerismo y responsabilidad, donde pueda avanzar con mi carrera profesional.</p>
                 <i> Ciudad de residencia: Mar del Plata.  </i>
@@ -41,7 +48,8 @@
             </div>
             <div class="border-resume col-md-5 offset-md-1 mt-1">
 
-            <ul class = "pl-0 list-style-none  pt-4">
+                <p class ="card-subtitle">SKILLS</p><hr>
+                <ul class = "pl-0 list-style-none  pt-4">
                     <li><b> Mobile: </b> 223-155-968437 </li>
                     <li><b> Email: </b> briascojazmin@gmail.com</li>
                     <li><b> Portfolio: </b> jazmin-briasco.herokuapp.com/ </li>
@@ -53,6 +61,7 @@
         
         <div class="row ">
             <div class="border-resume col-md-5 mt-1 ">
+                <p class ="card-subtitle">SKILLS</p><hr>
                 <p>Trabajo práctico integrador de Metodología en Sistemas I y Laboratorio IV, Universidad Tecnológica Nacional, 2021.</p>
                 <p>Sistema de venta de entradas de cine.</p>
                 <p>Desarrollo de clases, controladores, vistas y capa de acceso a datos. <p>
@@ -60,7 +69,8 @@
                 
                 
             </div>
-            <div class="border-resume col-md-5 offset-md-1 mt-1 pt-5">
+            <div class="border-resume col-md-5 offset-md-1 mt-1 ">
+            <p class ="card-subtitle">SKILLS</p><hr>
                 <p class="pt-3">Página personal realizada en PHP a modo de currículum:</p>
                 <p><a href="https://jazmin-briasco.herokuapp.com//" class="a-link" target="_blank">jazmin-briasco.com/</a></p>
                 <a class="a-link" href="/jazmin-personal/Views/img/jazmin-briasco-resume.pdf" download="jazmin-briasco-resume">
